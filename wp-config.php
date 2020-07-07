a25e3c40f7de318d8ee46e2a46b511329fb0a2c6
Gabor Kovacs <kovacs@h-sechs.de>
2020-07-07 21:08:16 +0200

    live Datenbank eingetragen

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp_forelleonline2' );
+define( 'DB_NAME', 'usr_p555593_1' );
 
 /** MySQL database username */
-define( 'DB_USER', 'wp_forelleonline2' );
+define( 'DB_USER', 'p555593d1' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'wp_forelleonline2' );
+define( 'DB_PASSWORD', '3j6aL#a,lxedrz' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db1394.mydbserver.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
